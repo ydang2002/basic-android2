26223aa2234e7c16c2e443a875229f7abbef0adc
LAPTOP-38ED4DO3\ASUS <nhuy.danghuynh2002@gmail.com>
2022-03-07 16:23:43 +0700

    Bài 10 tìm hiểu về viewGroup RelativeLayout trong android

---
+++ android-beginner/Bai10ThietKeGiaoDien/app/src/main/java/com/nhuy/bai10thietkegiaodien/MainActivity.java
@@ -0,0 +1,14 @@
+package com.nhuy.bai10thietkegiaodien;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
