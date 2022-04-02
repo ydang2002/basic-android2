1cef771c7b998a495f0f51e381b9c5117cf67180
LAPTOP-38ED4DO3\ASUS <nhuy.danghuynh2002@gmail.com>
2022-04-02 11:07:12 +0700

    Thiết kế calss Manactivity.java và activity_main.xml

--- AndroidLists/Lesson14FolfingCellWithRecyclerView/app/src/main/java/com/nhuy/lesson14folfingcellwithrecyclerview/MainActivity.java
+++ AndroidLists/Lesson14FolfingCellWithRecyclerView/app/src/main/java/com/nhuy/lesson14folfingcellwithrecyclerview/MainActivity.java
@@ -1,14 +1,43 @@
 package com.nhuy.lesson14folfingcellwithrecyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    //b1: khai báo
+    private RecyclerView rcvItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //b2: Thực hiện ánh xạ view
+        rcvItems = findViewById(R.id.rcv_item);
+
+        //b3: Xác định LinnearlayoutManager
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        rcvItems.setLayoutManager(linearLayoutManager);
+
+        //b4: khởi tạo itemAdapter
+        ItemAdapter itemAdapter = new ItemAdapter();
+        itemAdapter.setData(getListItems());
+        rcvItems.setAdapter(itemAdapter);
+    }
+
+    //b5 Viết hàm getData
+    private List<ItemObject> getListItems(){
+        List<ItemObject> list = new ArrayList<>();
+        for (int i = 0; i < 20; i++){
+            list.add(new ItemObject("Title item" + i, "Content item" +i));
+        }
+        return list;
     }
 }
