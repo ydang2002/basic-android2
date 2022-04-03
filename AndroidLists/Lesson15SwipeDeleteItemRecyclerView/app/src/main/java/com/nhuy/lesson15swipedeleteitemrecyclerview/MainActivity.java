b2713d63299e6f6b783c4e6a094cf7abc4ca7a03
LAPTOP-38ED4DO3\ASUS <nhuy.danghuynh2002@gmail.com>
2022-04-03 14:22:55 +0700

    Thiết kế layout_item

--- AndroidLists/Lesson15SwipeDeleteItemRecyclerView/app/src/main/java/com/nhuy/lesson15swipedeleteitemrecyclerview/MainActivity.java
+++ AndroidLists/Lesson15SwipeDeleteItemRecyclerView/app/src/main/java/com/nhuy/lesson15swipedeleteitemrecyclerview/MainActivity.java
@@ -1,11 +1,13 @@
 package com.nhuy.lesson15swipedeleteitemrecyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView rcvItem;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
