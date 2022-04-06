b6c9dbb4378dd97c187d455ae83b237bb2eb0e51
LAPTOP-38ED4DO3\ASUS <nhuy.danghuynh2002@gmail.com>
2022-04-06 15:28:50 +0700

    Xử lí phân trang

--- AndroidLists/Lesson17PaginationRecyclerViewType2/app/src/main/java/com/nhuy/lesson17paginationrecyclerviewtype2/MainActivity.java
+++ AndroidLists/Lesson17PaginationRecyclerViewType2/app/src/main/java/com/nhuy/lesson17paginationrecyclerviewtype2/MainActivity.java
@@ -7,7 +7,10 @@ import androidx.recyclerview.widget.RecyclerView;
 
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +19,14 @@ public class MainActivity extends AppCompatActivity {
     //b2 Khai báo
     private UserAdapter userAdapter;
     private List<User> mListUser;
+
+    //b5 khai báo
+    private boolean isLoading;
+    private boolean isLastPage;
+    private int totalPage = 5; //khởi tạo 5 trang
+    private int currentPage = 1;//xem page là bao nhieu
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,5 +45,78 @@ public class MainActivity extends AppCompatActivity {
         //b5 sử lí đường phân cách giữa các item
         RecyclerView.ItemDecoration itemDecoration = new DividerItemDecoration(this,DividerItemDecoration.VERTICAL);
         rcvUser.addItemDecoration(itemDecoration);
+
+        setFirstData();
+        //b9
+        rcvUser.addOnScrollListener((new PaginationScrollListener(linearLayoutManager) {
+            //b10 xử lí phân trnang
+            @Override
+            public void loadMoreItem() {
+                isLoading = true;
+
+                currentPage += 1;
+                loadNextPage();
+            }
+
+            @Override
+            public boolean isLoading() {
+                return isLoading;
+            }
+
+            @Override
+            public boolean isLoadPage() {
+                return isLastPage;
+            }
+        }));
+
+    }
+
+    //b6 Load data page 1
+    private void setFirstData(){
+        mListUser = getListUser();
+        userAdapter.setData(mListUser);
+
+        //b8 nếu chưa đến trang cuối cùng
+        if(currentPage < totalPage){
+            userAdapter.addFooterLoading();
+        } else{
+            isLastPage =true;
+        }
+    }
+
+    //b11 viết hàm
+    private void loadNextPage(){
+        //b12 khoảng thời gian load trang
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //gọi hàmlấy 10 item
+                List<User> list = getListUser();
+
+                userAdapter.removeFooterLoading();
+                mListUser.addAll(list);
+                userAdapter.notifyDataSetChanged();
+
+                //khi load xong
+                isLoading = false;
+                if(currentPage < totalPage){
+                    userAdapter.addFooterLoading();
+                } else{
+                    isLastPage =true;
+                }
+            }
+        }, 2000);
+
+    }
+
+    //b7 hàm get dữ liệu
+    private  List<User> getListUser(){
+        //b13 Hiển thị thông báo cho người dùng biết đang load trang nào
+        Toast.makeText(this,"Load data page " + currentPage,Toast.LENGTH_SHORT).show();
+        List<User> list = new ArrayList<>();
+        for(int i = 1; i <= 10; i++){
+            list.add(new User("User name "));
+        }
+        return list;
     }
 }
