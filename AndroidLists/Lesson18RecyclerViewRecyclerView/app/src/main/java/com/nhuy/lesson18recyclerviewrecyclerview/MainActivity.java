3de3a39e2f043a52e6a3147bdbe1711835ccc644
LAPTOP-38ED4DO3\ASUS <nhuy.danghuynh2002@gmail.com>
2022-04-15 20:11:15 +0700

    no message

--- AndroidLists/Lesson18RecyclerViewRecyclerView/app/src/main/java/com/nhuy/lesson18recyclerviewrecyclerview/MainActivity.java
+++ AndroidLists/Lesson18RecyclerViewRecyclerView/app/src/main/java/com/nhuy/lesson18recyclerviewrecyclerview/MainActivity.java
@@ -1,10 +1,13 @@
 package com.nhuy.lesson18recyclerviewrecyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView rcvDta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
