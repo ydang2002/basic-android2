7d6c601fff301e3ff39d565d945d5cb6e5cad07a
LAPTOP-38ED4DO3\ASUS <nhuy.danghuynh2002@gmail.com>
2022-07-23 22:01:25 +0700

    BroadcastReceiver trong Android Part 1 (Static Receivers)

---
+++ BroadcastReceiver/BroadcastReceiverPart1/app/src/main/java/com/nhuy/broadcastreceiverpart1/ExampleBroadcastReceiver.java
@@ -0,0 +1,45 @@
+package com.nhuy.broadcastreceiverpart1;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
+import android.net.NetworkInfo;
+import android.os.Build;
+import android.widget.Toast;
+
+public class ExampleBroadcastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if(ConnectivityManager.CONNECTIVITY_ACTION.equals(intent.getAction())){
+            if(idNetWorkAvailable(context)){
+                Toast.makeText(context, "Internet connected", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(context, "Internet Disconnected", Toast.LENGTH_LONG).show();
+            }
+        }
+
+    }
+
+    private boolean idNetWorkAvailable(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if(connectivityManager == null){
+            return false;
+        }
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            Network network = connectivityManager.getActiveNetwork();
+            if(network == null){
+                return false;
+            }
+
+            NetworkCapabilities capabilities = connectivityManager.getNetworkCapabilities(network);
+            return capabilities != null && capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI);
+        } else {
+            NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+            return networkInfo != null && networkInfo.isConnected();
+        }
+    }
+}
