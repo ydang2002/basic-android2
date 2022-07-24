3f7f8c858efe590ba6cea50b231d8e704434a9ff
LAPTOP-38ED4DO3\ASUS <nhuy.danghuynh2002@gmail.com>
2022-07-24 09:37:57 +0700

    BroadcastReceiver trong Android Part 2 (Dynamic Receivers)

--- BroadcastReceiver/BroadcastReceiverPart1/app/src/main/java/com/nhuy/broadcastreceiverpart1/MainActivity.java
+++ BroadcastReceiver/BroadcastReceiverPart1/app/src/main/java/com/nhuy/broadcastreceiverpart1/MainActivity.java
@@ -2,13 +2,39 @@ package com.nhuy.broadcastreceiverpart1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.text.InputFilter;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ExampleBroadcastReceiver exampleBroadcastReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        exampleBroadcastReceiver = new ExampleBroadcastReceiver();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        IntentFilter inputFilter = new IntentFilter(ConnectivityManager.EXTRA_NO_CONNECTIVITY);
+        registerReceiver(exampleBroadcastReceiver, inputFilter);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        //unregisterReceiver(exampleBroadcastReceiver);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(exampleBroadcastReceiver);
     }
 }
