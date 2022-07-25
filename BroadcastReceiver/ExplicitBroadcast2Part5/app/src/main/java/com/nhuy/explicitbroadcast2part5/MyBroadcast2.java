05f98b3284ec904b6d721448efb9c7f6dc86550a
LAPTOP-38ED4DO3\ASUS <nhuy.danghuynh2002@gmail.com>
2022-07-25 08:00:08 +0700

    BroadcastReceiver trong Android Part 5 (Explicit Broadcasts) project 2

--- BroadcastReceiver/ExplicitBroadcast2Part5/app/src/main/java/com/nhuy/explicitbroadcast2part5/MyBroadcast2.java
+++ BroadcastReceiver/ExplicitBroadcast2Part5/app/src/main/java/com/nhuy/explicitbroadcast2part5/MyBroadcast2.java
@@ -1,4 +1,17 @@
 package com.nhuy.explicitbroadcast2part5;
 
-public class MyBroadcast2 {
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class MyBroadcast2 extends BroadcastReceiver {
+
+    private static final String MY_TEXT = "com.Nhuy.MY_TEXT";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String myText = "MyBroadcast2" + intent.getStringExtra(MY_TEXT);
+        Toast.makeText(context, myText, Toast.LENGTH_SHORT).show();
+    }
 }
