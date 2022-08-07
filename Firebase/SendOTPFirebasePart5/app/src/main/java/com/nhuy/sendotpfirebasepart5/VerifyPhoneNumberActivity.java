2f0203c6997ab1f1a7fdfcd0515e59c16e1077d0
LAPTOP-38ED4DO3\ASUS <nhuy.danghuynh2002@gmail.com>
2022-08-07 09:59:42 +0700

    Logic xử lý màn hình VerifyPhoneNumberActivity

--- Firebase/SendOTPFirebasePart5/app/src/main/java/com/nhuy/sendotpfirebasepart5/VerifyPhoneNumberActivity.java
+++ Firebase/SendOTPFirebasePart5/app/src/main/java/com/nhuy/sendotpfirebasepart5/VerifyPhoneNumberActivity.java
@@ -1,16 +1,36 @@
 package com.nhuy.sendotpfirebasepart5;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class VerifyPhoneNumberActivity extends AppCompatActivity {
 
+    private static final String TAG = VerifyPhoneNumberActivity.class.getName();
     private EditText edtPhoneNumber;
     private Button btnVerifyPhoneNumber;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +39,9 @@ public class VerifyPhoneNumberActivity extends AppCompatActivity {
 
         setTileToolbar();
         initUi();
+
+        mAuth = FirebaseAuth.getInstance();
+
         btnVerifyPhoneNumber.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -26,10 +49,6 @@ public class VerifyPhoneNumberActivity extends AppCompatActivity {
                 onClickVerifyPhoneNumber(strPhoneNumber);
             }
         });
-    }
-
-    private void onClickVerifyPhoneNumber(String strPhoneNumber) {
-
     }
 
     private void setTileToolbar(){
@@ -41,5 +60,70 @@ public class VerifyPhoneNumberActivity extends AppCompatActivity {
     private void initUi(){
         edtPhoneNumber = findViewById(R.id.edt_phone_number);
         btnVerifyPhoneNumber = findViewById(R.id.btn_verify_phone_number);
+    }
+
+    private void onClickVerifyPhoneNumber(String strPhoneNumber) {
+        PhoneAuthOptions options =
+                PhoneAuthOptions.newBuilder(mAuth)
+                        .setPhoneNumber(strPhoneNumber)       // Phone number to verify
+                        .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+                        .setActivity(this)                 // Activity (for callback binding)
+                        .setCallbacks(new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                            @Override
+                            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                                signInWithPhoneAuthCredential(phoneAuthCredential);
+                            }
+
+                            @Override
+                            public void onVerificationFailed(@NonNull FirebaseException e) {
+                                Toast.makeText(VerifyPhoneNumberActivity.this, "Verification Failed", Toast.LENGTH_SHORT).show();
+                            }
+
+                            //Khi firebase gửi mã OTP thì nhảy vào hàm này
+                            @Override
+                            public void onCodeSent(@NonNull String verificationID, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                                super.onCodeSent(verificationID, forceResendingToken);
+                                goToEnterOtpActivity(strPhoneNumber, verificationID);
+                            }
+                        })
+                        .build();
+        PhoneAuthProvider.verifyPhoneNumber(options);
+    }
+
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.e(TAG, "signInWithCredential:success");
+
+                            FirebaseUser user = task.getResult().getUser();
+                            // Update UI
+                            goToMainActivity(user.getPhoneNumber());
+                        } else {
+                            // Sign in failed, display a message and update the UI
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
+                                // The verification code entered was invalid
+                                Toast.makeText(VerifyPhoneNumberActivity.this, "The verification code entered was invalid", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+                });
+    }
+
+    private void goToMainActivity(String phoneNumber) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("phone_number", phoneNumber);
+        startActivity(intent);
+    }
+
+    private void goToEnterOtpActivity(String strPhoneNumber, String verificationID) {
+        Intent intent = new Intent(this, EnterOTPActivity.class);
+        intent.putExtra("phone_number", strPhoneNumber);
+        intent.putExtra("verification_id", verificationID);
+        startActivity(intent);
     }
 }
