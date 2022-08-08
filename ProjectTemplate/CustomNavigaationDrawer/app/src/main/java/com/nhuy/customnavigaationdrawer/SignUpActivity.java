8c80852be1ecbdca207970bb9205a4d85c0915c1
LAPTOP-38ED4DO3\ASUS <nhuy.danghuynh2002@gmail.com>
2022-08-08 16:05:26 +0700

    Register user with Email and Password on Firebase

--- ProjectTemplate/CustomNavigaationDrawer/app/src/main/java/com/nhuy/customnavigaationdrawer/SignUpActivity.java
+++ ProjectTemplate/CustomNavigaationDrawer/app/src/main/java/com/nhuy/customnavigaationdrawer/SignUpActivity.java
@@ -1,11 +1,21 @@
 package com.nhuy.customnavigaationdrawer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -37,5 +47,26 @@ public class SignUpActivity extends AppCompatActivity {
     }
 
     private void onClickSignUp() {
+        String strEmail = edtEmail.getText().toString().trim();
+        String srtPassword = edtPassword.getText().toString().trim();
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        auth.createUserWithEmailAndPassword(strEmail, srtPassword)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                            startActivity(intent);
+                            //sau khi vào MainActivity, thực hiện đóng SignIn và SignUp
+                            finishAffinity();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(SignUpActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
     }
 }
