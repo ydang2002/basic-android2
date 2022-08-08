ef19f64d41fe910458bcd3b05bc3f14b0857d60c
LAPTOP-38ED4DO3\ASUS <nhuy.danghuynh2002@gmail.com>
2022-08-08 14:01:56 +0700

    Locgic xử lý màn hình EnterOtpActivity

--- Firebase/SendOTPFirebasePart5/app/src/main/java/com/nhuy/sendotpfirebasepart5/EnterOTPActivity.java
+++ Firebase/SendOTPFirebasePart5/app/src/main/java/com/nhuy/sendotpfirebasepart5/EnterOTPActivity.java
@@ -1,26 +1,53 @@
 package com.nhuy.sendotpfirebasepart5;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class EnterOTPActivity extends AppCompatActivity {
 
+    public static final String TAG = EnterOTPActivity.class.getName();
     private EditText edtOtp;
     private Button btnSendOtpCode;
     private TextView tvSendOtpAgain;
+
+    private FirebaseAuth mAuth;
+    private String mPhoneNumber;
+    private String mVerificationId;
+    private PhoneAuthProvider.ForceResendingToken mForceResendingToken;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter_otpactivity);
 
+        getDataIntent();
         setTileToolbar();
         initUi();
+
+        mAuth = FirebaseAuth.getInstance();
+
         btnSendOtpCode.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -37,6 +64,11 @@ public class EnterOTPActivity extends AppCompatActivity {
         });
     }
 
+    private void getDataIntent(){
+        mPhoneNumber = getIntent().getStringExtra("phone_number");
+        mVerificationId = getIntent().getStringExtra("verification_id");
+    }
+
     private void setTileToolbar(){
         if(getSupportActionBar() != null) {
             getActionBar().setTitle("Enter OTP Code");
@@ -50,8 +82,67 @@ public class EnterOTPActivity extends AppCompatActivity {
     }
 
     private void onClickOtpCode(String strOtp) {
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(mVerificationId, strOtp);
+        signInWithPhoneAuthCredential(credential);
     }
 
     private void onClickSendOtpAgain() {
+        PhoneAuthOptions options =
+                PhoneAuthOptions.newBuilder(mAuth)
+                        .setPhoneNumber(mPhoneNumber)       // Phone number to verify
+                        .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+                        .setActivity(this)                 // Activity (for callback binding)
+                        .setForceResendingToken(mForceResendingToken)
+                        .setCallbacks(new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                            @Override
+                            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                                signInWithPhoneAuthCredential(phoneAuthCredential);
+                            }
+
+                            @Override
+                            public void onVerificationFailed(@NonNull FirebaseException e) {
+                                Toast.makeText(EnterOTPActivity.this, "Verification Failed", Toast.LENGTH_SHORT).show();
+                            }
+
+                            //Khi firebase gửi mã OTP thì nhảy vào hàm này
+                            @Override
+                            public void onCodeSent(@NonNull String verificationID, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                                super.onCodeSent(verificationID, forceResendingToken);
+                                mVerificationId = verificationID;
+                                mForceResendingToken = forceResendingToken;
+                            }
+                        })
+                        .build();
+        PhoneAuthProvider.verifyPhoneNumber(options);
+    }
+
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.e(TAG, "signInWithCredential:success");
+
+                            FirebaseUser user = task.getResult().getUser();
+                            // Update UI
+                            goToMainActivity(user.getPhoneNumber());
+                        } else {
+                            // Sign in failed, display a message and update the UI
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
+                                // The verification code entered was invalid
+                                Toast.makeText(EnterOTPActivity.this, "The verification code entered was invalid", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+                });
+    }
+
+    private void goToMainActivity(String phoneNumber) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("phone_number", phoneNumber);
+        startActivity(intent);
     }
 }
--- Firebase/SendOTPFirebasePart5/app/src/main/java/com/nhuy/sendotpfirebasepart5/MainActivity.java
+++ Firebase/SendOTPFirebasePart5/app/src/main/java/com/nhuy/sendotpfirebasepart5/MainActivity.java
@@ -3,6 +3,7 @@ package com.nhuy.sendotpfirebasepart5;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +12,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getDataIntent();
         setTileToolbar();
+    }
+
+    private void getDataIntent(){
+        String strPhoneNumber = getIntent().getStringExtra("phone_number");
+
+        TextView tvUserInfor = findViewById(R.id.tv_user_infor);
+        tvUserInfor.setText(strPhoneNumber);
     }
 
     private void setTileToolbar(){
