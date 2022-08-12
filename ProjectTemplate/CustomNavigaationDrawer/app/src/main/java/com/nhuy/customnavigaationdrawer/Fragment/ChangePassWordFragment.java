c6efbe065c1d5ebbf38e6bf847cc4b9cab232571
LAPTOP-38ED4DO3\ASUS <nhuy.danghuynh2002@gmail.com>
2022-08-12 22:23:05 +0700

    Re-authenticate a user on Firebase

--- ProjectTemplate/CustomNavigaationDrawer/app/src/main/java/com/nhuy/customnavigaationdrawer/Fragment/ChangePassWordFragment.java
+++ ProjectTemplate/CustomNavigaationDrawer/app/src/main/java/com/nhuy/customnavigaationdrawer/Fragment/ChangePassWordFragment.java
@@ -15,6 +15,8 @@ import androidx.fragment.app.Fragment;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.nhuy.customnavigaationdrawer.R;
@@ -60,6 +62,28 @@ public class ChangePassWordFragment extends Fragment {
                         if (task.isSuccessful()) {
                             Toast.makeText(getActivity(), "User password updated.", Toast.LENGTH_SHORT).show();
                             progressDialog.dismiss();
+                        } else {
+                            // Show dialog re-Authenticate
+                        }
+                    }
+                });
+    }
+
+    private void reAuthenticate() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        AuthCredential credential = EmailAuthProvider
+                .getCredential("user@example.com", "password1234");
+
+// Prompt the user to re-provide their sign-in credentials
+        user.reauthenticate(credential)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            onClickChangePassword();
+                        } else {
+
                         }
                     }
                 });
