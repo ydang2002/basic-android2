932de5e96608a89ede7b7c1460d5e841f26af7a4
LAPTOP-38ED4DO3\ASUS <nhuy.danghuynh2002@gmail.com>
2022-08-18 09:23:30 +0700

    Write/Read 1 String tu Android Realtime Database

--- Firebase/RealtimeDatabasePart13/app/src/main/java/com/nhuy/realtimedatabasepart13/MainActivity.java
+++ Firebase/RealtimeDatabasePart13/app/src/main/java/com/nhuy/realtimedatabasepart13/MainActivity.java
@@ -3,12 +3,71 @@ package com.nhuy.realtimedatabasepart13;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+    TextView tvData;
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.edt_data);
+        Button btnPushData = findViewById(R.id.btn_push_data);
+        tvData = findViewById(R.id.tv_get_data);
+        Button btnGetData = findViewById(R.id.btn_get_data);
+
+        btnPushData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onClickPushData();
+            }
+        });
+
+        btnGetData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                readDataBase();
+            }
+        });
+    }
+
+    private void onClickPushData() {
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue(editText.getText().toString().trim());
+    }
+
+    private void readDataBase() {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                tvData.setText(value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+            }
+        });
     }
 }
