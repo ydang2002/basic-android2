e8a51dbcb1eaaf7c35b5228f67d5ce1c9d036631
LAPTOP-38ED4DO3\ASUS <nhuy.danghuynh2002@gmail.com>
2022-09-04 09:43:19 +0700

    Thêm một List (Object) trên Android Realtime Database  Part 21

--- Firebase/RealtimeDatabasePart13/app/src/main/java/com/nhuy/realtimedatabasepart13/MainActivity.java
+++ Firebase/RealtimeDatabasePart13/app/src/main/java/com/nhuy/realtimedatabasepart13/MainActivity.java
@@ -19,167 +19,91 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-    TextView tvData;
+    private EditText edtID, edtName;
+    private Button btnAddUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btnPushData = findViewById(R.id.btn_push_data);
-        tvData = findViewById(R.id.tv_get_data);
-        Button btnGetData = findViewById(R.id.btn_get_data);
-        Button btnDelete = findViewById(R.id.btn_delete_data);
-        Button btnUpdate = findViewById(R.id.btn_update_data);
+        initUi();
 
-        btnPushData.setOnClickListener(new View.OnClickListener() {
+        btnAddUser.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                onClickPushData();
-            }
-        });
-
-        btnGetData.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                onClickGetData();
-            }
-        });
-
-        btnDelete.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                onClickDeleteData();
-            }
-        });
-
-        btnUpdate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                onClickUpdateData();
+//                int id = Integer.parseInt(edtID.getText().toString().trim());
+//                String name = edtName.getText().toString().trim();
+//                User user = new User(id, name);
+//                onClickAddUser(user);
+                clickAddAllUser();
             }
         });
     }
 
-    private void onClickPushData() {
+    private void initUi() {
+        edtID = findViewById(R.id.edt_id);
+        edtName = findViewById(R.id.edt_name);
+        btnAddUser = findViewById(R.id.btn_add_user);
+    }
+
+    private void onClickAddUser(User user) {
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("my_map");
+        DatabaseReference myRef = database.getReference("list_users");
 
-        Map<String, Boolean> map = new HashMap<>();
-        map.put("1", true);
-        map.put("2", false);
-        map.put("3", true);
-        map.put("4", false);
-
-        myRef.setValue(map, new DatabaseReference.CompletionListener() {
+        String pathObject = String.valueOf(user.getId());
+        myRef.child(pathObject).setValue(user, new DatabaseReference.CompletionListener() {
             @Override
             public void onComplete(@Nullable DatabaseError error, @NonNull DatabaseReference ref) {
-                Toast.makeText(MainActivity.this, "Push data success", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Add user success", Toast.LENGTH_SHORT).show();
             }
         });
     }
 
-    private void onClickGetData() {
-
+    private void clickAddAllUser() {
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("my_map");
-        // Read from the database
-        myRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-               Map<String, Boolean> mapResult = new HashMap<>();
-               for (DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()) {
-                   String key = dataSnapshot1.getKey();
-                   Boolean value = dataSnapshot1.getValue(Boolean.class);
+        DatabaseReference myRef = database.getReference("list_users");
 
-                   mapResult.put(key, value);
-               }
-                tvData.setText(mapResult.toString());
-            }
+        List<User> list = new ArrayList<>();
+        list.add(new User(1, "User1"));
+        list.add(new User(2, "User2"));
+        list.add(new User(3, "User3"));
 
+        myRef.setValue(list, new DatabaseReference.CompletionListener() {
             @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
+            public void onComplete(@Nullable DatabaseError error, @NonNull DatabaseReference ref) {
+                Toast.makeText(MainActivity.this, "Add all user success", Toast.LENGTH_SHORT).show();
             }
         });
     }
 
-    private void onClickUpdateData() {
 
-        // Cách 1
-//        FirebaseDatabase database = FirebaseDatabase.getInstance();
-//        DatabaseReference myRef = database.getReference("my_map");
+//    private void onClickDeleteData() {
+//        new AlertDialog.Builder(this)
+//                .setTitle(getString(R.string.app_name))
+//                .setMessage("Bạn có chắc chắn xóa không?")
+//                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialogInterface, int i) {
+//                        FirebaseDatabase database = FirebaseDatabase.getInstance();
+//                        DatabaseReference myRef = database.getReference("my_map");
 //
-//        Map<String, Boolean> mapUpdate = new HashMap<>();
-//        mapUpdate.put("1", true);
-//        mapUpdate.put("2", false);
-//        mapUpdate.put("3", true);
-//        mapUpdate.put("4", false);
-//
-//        myRef.setValue(mapUpdate, new DatabaseReference.CompletionListener() {
-//            @Override
-//            public void onComplete(@Nullable DatabaseError error, @NonNull DatabaseReference ref) {
-//                Toast.makeText(MainActivity.this, "Update data success", Toast.LENGTH_SHORT).show();
-//            }
-//        });
-
-
-//        // Cách 3
-//        FirebaseDatabase database = FirebaseDatabase.getInstance();
-//        DatabaseReference myRef = database.getReference("my_map");
-//        Map<String, Object> mapUpdate = new HashMap<>();
-//        mapUpdate.put("2", "true");
-//        mapUpdate.put("4", "true");
-//
-//        myRef.updateChildren(mapUpdate, new DatabaseReference.CompletionListener() {
-//            @Override
-//            public void onComplete(@Nullable DatabaseError error, @NonNull DatabaseReference ref) {
-//                Toast.makeText(MainActivity.this, "Update data success", Toast.LENGTH_SHORT).show();
-//            }
-//        });
-    }
-
-    private void onClickDeleteData() {
-        new AlertDialog.Builder(this)
-                .setTitle(getString(R.string.app_name))
-                .setMessage("Bạn có chắc chắn xóa không?")
-                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        FirebaseDatabase database = FirebaseDatabase.getInstance();
-                        DatabaseReference myRef = database.getReference("my_map");
-
-                        // Cách 1
 //                        myRef.removeValue(new DatabaseReference.CompletionListener() {
-//                            @Override
-//                            public void onComplete(@Nullable DatabaseError error, @NonNull DatabaseReference ref) {
-//                                Toast.makeText(MainActivity.this, "Delete data success", Toast.LENGTH_SHORT).show();
-//                            }
-//                        });
-
-                        // Cách 2
-//                        myRef.child("job").child("name").removeValue(new DatabaseReference.CompletionListener() {
 //                            @Override
 //                            public void onComplete(@Nullable DatabaseError error, @NonNull DatabaseReference ref) {
 //                                Toast.makeText(MainActivity.this, "Update data success", Toast.LENGTH_SHORT).show();
 //                            }
 //                        });
-
-                        // Xóa Object user_info
-                        myRef.removeValue(new DatabaseReference.CompletionListener() {
-                            @Override
-                            public void onComplete(@Nullable DatabaseError error, @NonNull DatabaseReference ref) {
-                                Toast.makeText(MainActivity.this, "Update data success", Toast.LENGTH_SHORT).show();
-                            }
-                        });
-                    }
-                })
-                .setNegativeButton("Cancel", null)
-                .show();
-    }
+//                    }
+//                })
+//                .setNegativeButton("Cancel", null)
+//                .show();
+//    }
 }
--- Firebase/RealtimeDatabasePart13/app/src/main/java/com/nhuy/realtimedatabasepart13/User.java
+++ Firebase/RealtimeDatabasePart13/app/src/main/java/com/nhuy/realtimedatabasepart13/User.java
@@ -8,22 +8,18 @@ import java.util.Map;
 public class User {
     private int id;
     private String name;
-    private Job job;
-    private String address;
 
     public User() {
     }
 
-    public User(int id, String name, Job job) {
+    public User(int id, String name) {
         this.id = id;
         this.name = name;
-        this.job = job;
     }
 
     //constructor dùng cho update
     public User(String name, String address) {
         this.name = name;
-        this.address = address;
     }
 
     public int getId() {
@@ -42,36 +38,18 @@ public class User {
         this.name = name;
     }
 
-    public Job getJob() {
-        return job;
-    }
-
-    public void setJob(Job job) {
-        this.job = job;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
 
     @Override
     public String toString() {
         return "User{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
-                ", job=" + job +
-                ", address='" + address + '\'' +
                 '}';
     }
 
     @Exclude
     public Map<String, Object> toMap() {
         HashMap<String, Object> result = new HashMap<>();
-        result.put("address", address);
         result.put("name", name);
 
         return result;
