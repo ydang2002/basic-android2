f39dbf81930f2d9d4d50ba7049475cefa8448817
LAPTOP-38ED4DO3\ASUS <nhuy.danghuynh2002@gmail.com>
2022-09-06 06:53:30 +0700

    Sử dụng ChildEventListener get List Object from Android Realtime Database part 23

--- Firebase/RealtimeDatabasePart13/app/src/main/java/com/nhuy/realtimedatabasepart13/MainActivity.java
+++ Firebase/RealtimeDatabasePart13/app/src/main/java/com/nhuy/realtimedatabasepart13/MainActivity.java
@@ -3,6 +3,9 @@ package com.nhuy.realtimedatabasepart13;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
@@ -10,9 +13,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -27,6 +32,9 @@ import java.util.Map;
 public class MainActivity extends AppCompatActivity {
     private EditText edtID, edtName;
     private Button btnAddUser;
+    private RecyclerView rcvUsers;
+    private UserAdapter mUserAdapter;
+    private List<User> mListUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,19 +46,33 @@ public class MainActivity extends AppCompatActivity {
         btnAddUser.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                int id = Integer.parseInt(edtID.getText().toString().trim());
-//                String name = edtName.getText().toString().trim();
-//                User user = new User(id, name);
-//                onClickAddUser(user);
-                clickAddAllUser();
+                int id = Integer.parseInt(edtID.getText().toString().trim());
+                String name = edtName.getText().toString().trim();
+                User user = new User(id, name);
+                onClickAddUser(user);
+                //clickAddAllUser();
             }
         });
+
+        getListUserFromRealtimeDatabase();
     }
 
     private void initUi() {
         edtID = findViewById(R.id.edt_id);
         edtName = findViewById(R.id.edt_name);
         btnAddUser = findViewById(R.id.btn_add_user);
+        rcvUsers = findViewById(R.id.rcv_users);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        rcvUsers.setLayoutManager(linearLayoutManager);
+
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+        rcvUsers.addItemDecoration(dividerItemDecoration);
+
+        mListUser = new ArrayList<>();
+        mUserAdapter = new UserAdapter(mListUser);
+
+        rcvUsers.setAdapter(mUserAdapter);
     }
 
     private void onClickAddUser(User user) {
@@ -80,6 +102,62 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onComplete(@Nullable DatabaseError error, @NonNull DatabaseReference ref) {
                 Toast.makeText(MainActivity.this, "Add all user success", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void getListUserFromRealtimeDatabase() {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("list_users");
+
+        // Cách 1
+//        myRef.addValueEventListener(new ValueEventListener() {
+//
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                if (mListUser != null) {
+//                    mListUser.clear();
+//                }
+//                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+//                    User user = dataSnapshot.getValue(User.class);
+//                    mListUser.add(user);
+//                }
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//                Toast.makeText(MainActivity.this, "Get list user failed", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+
+        myRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                User user = snapshot.getValue(User.class);
+                if (user != null) {
+                    mListUser.add(user);
+                    mUserAdapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
             }
         });
     }
